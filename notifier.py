d56cfaece7d14786f4fd4b2483143913ef663c12
agentnola <wade@nowarehouse.com>
2015-08-08 18:25:16 -0500

    Logic Update

--- notifier.py
+++ notifier.py
@@ -26,20 +26,20 @@ r.login(username,password,disable_warning=True)
 already_done = str(input.read()).split()
 
 #keywords to search, need to pull these from the yaml too. 
-tier1 = ['Looking to join', 'group of friends', 'new person']
-tier2 = ['joining',"settlement","large city","small city","large town","small town","start"]
+tier1 = ['Looking to join', 'group of friends', 'new person','joining',"settlement","large city","small city","large town","small town","start","complexity"]
+##tier2 = ['joining',"settlement","large city","small city","large town","small town","start","complexity"]
 
 #don't change below plz
 while True:
     print 'Checking for more newfriends...'
     subreddit = r.get_subreddit('civcraft')
-    for submission in subreddit.get_new(limit=10):
+    for submission in subreddit.get_new(limit=100):
         op_text = submission.selftext.lower()
         has_newfriend = False
         if any(string in op_text for string in tier1): has_newfriend = True
-        for x in range(len(tier2)):
-            if tier2[x] in op_text or submission.title:
-                if any(string in op_text for string in tier2) and not tier2[x]: has_newfriend = True
+#        for x in range(len(tier2)):
+#            if tier2[x] in op_text or submission.title:
+#                if any(string in op_text for string in tier2) and not tier2[x]: has_newfriend = True
 
         flair = submission.author_flair_text
         if submission.id not in already_done and has_newfriend and not flair:
